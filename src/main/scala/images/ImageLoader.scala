9bbe51380e521abb19b32b1c302f8bc9a5c4d16d
spf3000 <alandevlin7@gmail.com>
2019-05-13 20:00:23 +0100

    she's a beauty!

--- src/main/scala/images/ImageLoader.scala
+++ src/main/scala/images/ImageLoader.scala
@@ -2,15 +2,8 @@ package images
 import java.io.InputStream
 import scalaz.zio.Task
 import scalaz.zio.ZIO
-import java.io.OutputStream
 
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.model.ObjectMetadata
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import com.amazonaws.AmazonServiceException
-import java.io.File
-import com.amazonaws.regions.Region
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.s3.AmazonS3Client
 
@@ -20,22 +13,27 @@ trait ImageLoader {
 
 object ImageLoader {
   trait Service[R] {
-    def store(i: InputStream, id: String, bucketName: String, s3: AmazonS3): ZIO[R, Throwable, Unit]
+    def store(i: InputStream, id: String): ZIO[R, Throwable, Unit]
   }
 
-  trait S3Loader extends ImageLoader {
-    def imageLoader: ImageLoader.Service[Any] =
-      new Service[Any] {
-        def store(
-            i: InputStream,
-            id: String,
-            bucketName: String,
-            s3: AmazonS3
-        ): ZIO[Any, Throwable, Unit] = {
-          val putObjectRequest = new PutObjectRequest(bucketName, id, i, new ObjectMetadata())
-          ZIO.effect(s3.putObject(putObjectRequest))
-        }
-      }
+  case class PutS3(s3: AmazonS3, put: PutObjectRequest)
+
+  def store(i: InputStream, id: String): ZIO[PutS3, Throwable, Unit] =
+    for {
+      s3      <- ZIO.access[PutS3](_.s3)
+      request <- ZIO.access[PutS3](_.put)
+    } yield s3.putObject(request)
+
+  class LiveS3Loader(p: PutS3) extends ImageLoader {
+    def imageLoader = new Service[Any] {
+      def store(i: InputStream, id: String): ZIO[Any, Throwable, Unit] =
+        store(i, id).provide(p)
+    }
   }
-  object S3Loader extends S3Loader
+
+  object imageLoader {
+    def store(i: InputStream, id: String): ZIO[ImageLoader, Throwable, Unit] =
+      ZIO.accessM(_.imageLoader.store(i, id))
+
+  }
 }
--- src/main/scala/images/LambdaFunc.scala
+++ src/main/scala/images/LambdaFunc.scala
@@ -1,28 +1,31 @@
 package images
+
 import java.io.InputStream
 import scalaz.zio.ZIO
 import scalaz.zio.internal.PlatformLive
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.{AmazonS3ClientBuilder, AmazonS3Client}
 import com.amazonaws.regions.Region
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.{ObjectMetadata, PutObjectRequest}
 
 import scalaz.zio.internal.PlatformLive
 import scalaz.zio.Runtime
-import scalaz.zio.DefaultRuntime
 
 object LambdaFunc {
   import ImageLoader._
 
   def run(i: InputStream, id: String, bucketName: String) = {
-    val s3Client = AmazonS3ClientBuilder.standard()
-              .withRegion("eu-west-1")
-              .withCredentials(new ProfileCredentialsProvider())
-              .build();
+    val s3Client = AmazonS3ClientBuilder
+      .standard()
+      .withRegion("eu-west-1")
+      .withCredentials(new ProfileCredentialsProvider())
+      .build();
+    val putRequest = new PutObjectRequest(bucketName, id, i, new ObjectMetadata())
 
-    val runTime = new DefaultRuntime {}
+    val liveS3Load = new LiveS3Loader(PutS3(s3Client, putRequest))
+    val runTime    = Runtime(liveS3Load, PlatformLive.Default)
     runTime.unsafeRun(
-      S3Loader.imageLoader.store(i,id,bucketName).provide(s3Client)
+      imageLoader.store(i, id)
     )
   }
 
